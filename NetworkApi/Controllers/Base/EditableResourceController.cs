4be389fa06cf84cdeb51431736cc0316dc345a82
daniel <daniel>
2016-08-11 15:07:14 +0700

    + Add metadata for entity

--- NetworkApi/Controllers/Base/EditableResourceController.cs
+++ NetworkApi/Controllers/Base/EditableResourceController.cs
@@ -20,70 +20,12 @@ namespace NetWorkApi.Controllers.Base
         where Entity: AppModel
     {
 
-
         /// <summary>
         /// 
         /// </summary>
         /// <param name="repository"></param>
         public EditableResourceController(IRepository<Entity> repository): base(repository)
         {
-        }
-
-
-        /// <summary>
-        /// Get validation constraints
-        /// </summary>
-        [HttpGet("metadata")]
-        public Dictionary<string, IEnumerable<DataValidation>> GetConstraints()
-        {
-            var result = new Dictionary<string, IEnumerable<DataValidation>>();
-            var type = typeof(Entity);
-            var properties = type.GetProperties();
-
-            foreach (var property in properties)
-            {
-                var attributes = property.GetCustomAttributes<Attribute>();
-                var validations = new List<DataValidation>();
-
-                foreach (var attribute in attributes)
-                {
-                    var emailAddressAttribute = attribute as EmailAddressAttribute;
-
-                    if (emailAddressAttribute != null) {
-                        validations.Add(new RegularExpressionValidation(
-                            "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,63}$",
-                            emailAddressAttribute.ErrorMessage
-                            ));
-                        break;
-                    }
-
-                    var rangeAttribute = attribute as RangeAttribute;
-                    if (rangeAttribute != null)
-                    {
-                        validations.Add(new RangeValidation(
-                            rangeAttribute.Minimum, 
-                            rangeAttribute.Maximum, 
-                            rangeAttribute.ErrorMessage));
-                        break;
-                    }
-
-                    var stringLengthAttribute = attribute as StringLengthAttribute;
-                    if (stringLengthAttribute != null)
-                    {
-                        validations.Add(new StringLengthValidation(
-                                stringLengthAttribute.MinimumLength,
-                                stringLengthAttribute.MaximumLength,
-                                stringLengthAttribute.ErrorMessage
-                            ));
-                        break;
-                    }
-
-                }
-
-                result.Add(property.Name, validations);
-            }
-
-            return result;
         }
 
         /// <summary>
--- NetworkApi/Controllers/Base/ResourceController.cs
+++ NetworkApi/Controllers/Base/ResourceController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NetWorkApi.Core.Model;
+using NetWorkApi.Core.Validations;
 using NetWorkApi.Models;
 using NetWorkApi.Repositories;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace NetWorkApi.Controllers
 {
@@ -74,6 +77,78 @@ namespace NetWorkApi.Controllers
                     isAccending
                 )
             );
+        }
+
+        /// <summary>
+        /// Get validation constraints
+        /// </summary>
+        [HttpGet("metadata")]
+        public IEnumerable<ColumnData> GetConstraints()
+        {
+            var result = new List<ColumnData>();
+
+            var type = typeof(Entity);
+            var properties = type.GetProperties();
+
+            foreach (var property in properties)
+            {
+                var columnData = new ColumnData
+                {
+                    Name = property.Name
+                };
+
+                var displayAttribute = property.GetCustomAttribute<DisplayAttribute>();
+
+                if (displayAttribute != null)
+                {
+                    columnData.DisplayName = displayAttribute.Name;
+                    columnData.Description = displayAttribute.Description;
+                }
+                    
+                var attributes = property.GetCustomAttributes<Attribute>();
+                var validations = new List<DataValidation>();
+
+                foreach (var attribute in attributes)
+                {
+                    var emailAddressAttribute = attribute as EmailAddressAttribute;
+
+                    if (emailAddressAttribute != null)
+                    {
+                        validations.Add(new RegularExpressionValidation(
+                            "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,63}$",
+                            emailAddressAttribute.ErrorMessage
+                            ));
+                        break;
+                    }
+
+                    var rangeAttribute = attribute as RangeAttribute;
+                    if (rangeAttribute != null)
+                    {
+                        validations.Add(new RangeValidation(
+                            rangeAttribute.Minimum,
+                            rangeAttribute.Maximum,
+                            rangeAttribute.ErrorMessage));
+                        break;
+                    }
+
+                    var stringLengthAttribute = attribute as StringLengthAttribute;
+                    if (stringLengthAttribute != null)
+                    {
+                        validations.Add(new StringLengthValidation(
+                                stringLengthAttribute.MinimumLength,
+                                stringLengthAttribute.MaximumLength,
+                                stringLengthAttribute.ErrorMessage
+                            ));
+                        break;
+                    }
+
+                }
+
+                columnData.Validations = validations;
+                result.Add(columnData);
+            }
+
+            return result;
         }
     }
 }
